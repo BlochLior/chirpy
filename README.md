e199113b004962790e248792f3c406dcc2064e02
BlochLior <blochlior@gmail.com>
2025-04-26 09:55:35 +0300

    done

---
+++ README.md
@@ -0,0 +1,4 @@
+# chirpy - guided boot.dev project
+this project is a guided project in the boot.dev projects.
+## what it does?
+it's a baseline for a social network that was once called twitter.
--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -2,52 +2,68 @@ package main
 
 import (
 	"net/http"
+	"sort"
 
 	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) handlerChirpsGet(w http.ResponseWriter, r *http.Request) {
+	chirpIDString := r.PathValue("chirpID")
+	chirpID, err := uuid.Parse(chirpIDString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "invalid chirp ID", err)
+		return
+	}
+
+	dbChirp, err := cfg.db.GetChirpByID(r.Context(), chirpID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "couldn't get chirp", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, Chirp{
+		ID:        dbChirp.ID,
+		CreatedAt: dbChirp.UpdatedAt,
+		UpdatedAt: dbChirp.UpdatedAt,
+		UserID:    dbChirp.UserID,
+		Body:      dbChirp.Body,
+	})
+}
+
+func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Request) {
 	dbChirps, err := cfg.db.GetChirps(r.Context())
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Unable to retrieve chirps from database", err)
+		respondWithError(w, http.StatusInternalServerError, "couldn't retrieve chirps", err)
 		return
+	}
+
+	authorID := uuid.Nil
+	authorIDString := r.URL.Query().Get("author_id")
+	if authorIDString != "" {
+		authorID, err = uuid.Parse(authorIDString)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "invalid author ID", err)
+			return
+		}
 	}
 	chirps := []Chirp{}
 	for _, dbChirp := range dbChirps {
+		if authorID != uuid.Nil && dbChirp.UserID != authorID {
+			continue
+		}
 		chirps = append(chirps, Chirp{
 			ID:        dbChirp.ID,
 			CreatedAt: dbChirp.CreatedAt,
 			UpdatedAt: dbChirp.UpdatedAt,
-			Body:      dbChirp.Body,
 			UserID:    dbChirp.UserID,
+			Body:      dbChirp.Body,
 		})
 	}
+	sortOpt := r.URL.Query().Get("sort")
+	if sortOpt == "desc" {
+		sort.Slice(chirps, func(i, j int) bool { return chirps[i].CreatedAt.After(chirps[j].CreatedAt) })
+	} else {
+		sort.Slice(chirps, func(i, j int) bool { return chirps[i].CreatedAt.Before(chirps[j].CreatedAt) })
+	}
 	respondWithJSON(w, http.StatusOK, chirps)
-}
-
-func (cfg *apiConfig) handlerChirpGet(w http.ResponseWriter, r *http.Request) {
-	chirpIDstr := r.PathValue("chirpID")
-	if chirpIDstr == "" {
-		respondWithError(w, http.StatusNotFound, "Chirp not found", nil)
-		return
-	}
-
-	chirpID, err := uuid.Parse(chirpIDstr)
-	if err != nil {
-		respondWithError(w, http.StatusNotFound, "Invalid chirpID", err)
-		return
-	}
-
-	chirp, err := cfg.db.GetChirpByID(r.Context(), chirpID)
-	if err != nil {
-		respondWithError(w, http.StatusNotFound, "encountered error trying to retrieve chirp by id", err)
-		return
-	}
-	respondWithJSON(w, http.StatusOK, Chirp{
-		ID:        chirp.ID,
-		CreatedAt: chirp.CreatedAt,
-		UpdatedAt: chirp.UpdatedAt,
-		Body:      chirp.Body,
-		UserID:    chirp.UserID,
-	})
 }
